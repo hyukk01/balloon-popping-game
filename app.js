65d5fcd043bc98b44ae75f3a85383b71e4c20f4c
sjkang <sjkang@daegu.ac.kr>
2024-05-07 17:56:23 +0900

    1st commit

--- app.js
+++ app.js
@@ -1,29 +1,37 @@
+let score = 0;
 let popped = 0;
 
-document.addEventListener('mouseover', function(e){
-    
-    if (e.target.className === "balloon"){
-        
-                e.target.style.backgroundColor = "#ededed";
-                e.target.textContent = "POP!";
-                popped++;
-                removeEvent(e);
-                checkAllPopped();
-    }   
+const balloons = document.querySelectorAll('.balloon');
+
+balloons.forEach(balloon => {
+    balloon.addEventListener('click', function(e) {
+        if (e.target.className === 'balloon') {
+            e.target.style.backgroundColor = '#ededed';
+            e.target.textContent = '펑!';
+            score += 10;
+            popped++;
+            updateScore();
+            checkAllPopped();
+            removeEvent(e);
+        }
+    });
 });
 
-function removeEvent(e){
-    e.target.removeEventListener('mouseover', function(){
-        
-    })
-};
+function updateScore() {
+    const scoreDisplay = document.getElementById('score');
+    scoreDisplay.textContent = `Score: ${score}`;
+}
 
-function checkAllPopped(){
-    if (popped === 24){
+function removeEvent(e) {
+    e.target.removeEventListener('click', function() {});
+}
+
+function checkAllPopped() {
+    if (popped === 24) {
         console.log('all popped!');
         let gallery = document.querySelector('#balloon-gallery');
         let message = document.querySelector('#yay-no-balloons');
         gallery.innerHTML = '';
         message.style.display = 'block';
     }
-};
+}
